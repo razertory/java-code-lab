c247c4c73e1942cb2111d5f08afcf4f3a9b4ce72
razertory <razertory@163.com>
2018-06-07 09:20:12 +0800

    修正一个结论性错误

--- src/test/java/org/razertory/datastructure/designPatterns/singleton/classic/SingletonTest.java
+++ src/test/java/org/razertory/datastructure/designPatterns/singleton/classic/SingletonTest.java
@@ -16,13 +16,12 @@ public class SingletonTest {
 
     /**
      * 非线程安全的单例模式，并发只执行获取对象方法的时候，可能会出现不同的对象（观察hashcode的值）。
-     * 请使用者注意，每次跑这个测试用例的时候，一定要确保字节码不存在了，也就是代码没有被编译过。
      * 原理参考：
      * http://www.hollischuang.com/archives/1860
      * http://www.hollischuang.com/archives/199
      */
     @Test
-    public void concurrentTestGetInstance() throws Exception {
+    public void concurrentGetInstance() throws Exception {
         Thread[] threads = new Thread[10];
         for (int i = 0; i < threads.length; i++) {
             threads[i] = new Thread(
