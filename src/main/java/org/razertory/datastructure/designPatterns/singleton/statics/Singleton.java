e35e5c0c7edbee1c8606e5136ff466e9794731df
razertory <razertory@163.com>
2018-06-07 09:32:09 +0800

    增加饿汉式的单例模式测试代码

--- src/main/java/org/razertory/datastructure/designPatterns/singleton/statics/Singleton.java
+++ src/main/java/org/razertory/datastructure/designPatterns/singleton/statics/Singleton.java
@@ -1,7 +1,8 @@
 package org.razertory.datastructure.designPatterns.singleton.statics;
 
 /**
- * 静态变量初始化实现线程安全的单例模式<br>
+ * 饿汉式
+ * 静态变量初始化实现线程安全的单例模式
  * static变量 instance 是在类被加载时初始化并仅被初始化一次，这样就可以保证只有一个instance被初始化
  * @author razertory
  *
--- src/test/java/org/razertory/datastructure/designPatterns/singleton/statics/SingletonTest.java
+++ src/test/java/org/razertory/datastructure/designPatterns/singleton/statics/SingletonTest.java
@@ -1,4 +1,4 @@
-package org.razertory.datastructure.designPatterns.singleton.classic;
+package org.razertory.datastructure.designPatterns.singleton.statics;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -14,12 +14,6 @@ public class SingletonTest {
         Assert.assertTrue(a.hashCode() == b.hashCode());
     }
 
-    /**
-     * 非线程安全的单例模式，并发执行获取对象方法的时候，可能会出现不同的对象（观察hashcode的值）。
-     * 原理参考：
-     * http://www.hollischuang.com/archives/1860
-     * http://www.hollischuang.com/archives/199
-     */
     @Test
     public void concurrentGetInstance() throws Exception {
         Thread[] threads = new Thread[10];
