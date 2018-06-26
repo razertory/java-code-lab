be1e540c5c0554e84b2005ddcbf5aa50baba1caa
razertory <razertory@163.com>
2018-06-26 15:16:29 +0800

    增加递归调用的方法和测试代码

--- src/test/java/org/razertory/datastructure/dp/CoinTest.java
+++ src/test/java/org/razertory/datastructure/dp/CoinTest.java
@@ -1,11 +1,28 @@
-package org.razertory.datastructure.dp; import org.junit.Test;
+package org.razertory.datastructure.dp; import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
 
 public class CoinTest {
+    int[] coins;
+    int value;
+
+    @Before
+    public void setup(){
+        this.coins = new int[]{5, 3, 4, 8, 6, 7};
+        this.value = 625;
+    }
 
     @Test
-    public void coin() {
+    public void coinDP() {
         Coin coin = new Coin();
-        int result = coin.coin(new int[]{5, 3, 4, 8, 6, 7}, 13);
-        System.out.println(result);
+        int result = coin.coinDP(coins, value);
+        Assert.assertEquals(79, result);
+    }
+
+    @Test
+    public void coinRecursion() {
+        Coin coin = new Coin();
+        int result = coin.coinRecursion(coins, coins.length, value);
+        Assert.assertEquals(79, result);
     }
 }
