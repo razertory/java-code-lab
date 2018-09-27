583a59887b8cba6bcf2d1a8048073d18daeb8815
awarker <1349028023@qq.com>
2018-09-27 10:44:58 +0800

    Update KMP.java
    //KMP加注释，修改格式

--- src/main/java/org/razertory/datastructure/string/KMP.java
+++ src/main/java/org/razertory/datastructure/string/KMP.java
@@ -2,23 +2,23 @@ package org.razertory.datastructure.string;
 
 /*
  * @Author Awarker
+ * KMP算法
+ * 应用场景：两个字符串匹配的情况
+ * 核心：KMP算法的关键是利用匹配失败后的信息，尽量减少子串与主串的匹配次数以达到快速匹配的目的
  */
 public class KMP {
+	//pat子串    txt  父串 
     public void kmpSearch(String pat, String txt) {
-        
-        String a = "ababa";
-        String b = "ssdfgasdbababx";
-        int[] next = get_next(a);
-        int res = kmp(b, a,next);
+        int[] next = get_next(pat);
+        int res = kmp(txt, pat,next);
         System.out.println(res);
         for(int i = 0; i < next.length; i++){
             System.out.print(" --> "+next[i]);            
         }
-        System.out.println();
         System.out.println(next.length);
     }
     
-    public  int kmp(String str, String dest,int[] next){//str被匹配串  dest 匹配串
+    public  int kmp(String str, String dest,int[] next){//str父串  dest 子串
         for(int i = 0, j = 0; i < str.length(); i++){
             while(j > 0 && str.charAt(i) != dest.charAt(j)){
                 j = next[j - 1];
