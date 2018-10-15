a9465fe6a78cee6741f9a66b4a30dd69facd0f13
awarker <1349028023@qq.com>
2018-10-15 23:42:42 +0800

    add KthBigest

--- src/main/java/org/razertory/datastructure/sort/KthBigest.java
+++ src/main/java/org/razertory/datastructure/sort/KthBigest.java
@@ -8,7 +8,36 @@ public class KthBigest {
      * @return: the Kth largest element
      */
     public int kthLargestElement(int n, int[] nums) {
-        // write your code here
-        return 0;
+        quickSort(0, nums.length-1, nums );
+        int key = nums.length - n;
+        return nums[key];
+    }
+
+
+    public static void quickSort(int lo, int hi, int[] nums) {
+    	if(lo > hi) {
+    		return;
+    	}
+        int index = pos(lo, hi, nums);
+        quickSort(lo, index-1, nums);
+        quickSort(index+1, hi, nums);
+
+    }
+
+    //获取中间位置
+    public static int pos(int lo, int hi, int[] nums){
+    	int key = nums[lo];
+        while(lo < hi){
+            while(nums[hi] >= key&& lo < hi){
+                hi--;
+            };
+            nums[lo] = nums[hi];
+            while(nums[lo] <= key&&lo <hi){
+                lo ++;
+            };
+            nums[hi] = nums[lo];
+        }
+        nums[hi] = key;
+        return hi;
     }
 }
