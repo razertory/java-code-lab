c4feecfe7a13345bc5572b330d1e85e5a5b6cd3d
chenlichun <razertory@163.com>
2018-10-15 14:31:53 +0800

    kth bigest

---
+++ src/main/java/org/razertory/datastructure/sort/KthBigest.java
@@ -0,0 +1,14 @@
+package org.razertory.datastructure.sort;
+
+//https://www.lintcode.com/problem/kth-largest-element/description
+public class KthBigest {
+    /**
+     * @param n: An integer
+     * @param nums: An array
+     * @return: the Kth largest element
+     */
+    public int kthLargestElement(int n, int[] nums) {
+        // write your code here
+        return 0;
+    }
+}
---
+++ src/test/java/org/razertory/datastructure/sort/KthBigestTest.java
@@ -0,0 +1,25 @@
+package org.razertory.datastructure.sort;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KthBigestTest {
+    private int index = 0;
+    private int []nums = null;
+    private KthBigest kthBigest = null;
+
+    @Before
+    public void setup() {
+       index = 3;
+       nums = new int[] {9,3,2,4,8};
+       kthBigest = new KthBigest();
+    }
+
+    @Test
+    public void kthLargestElement() {
+        Assert.assertEquals(4, kthBigest.kthLargestElement(index, nums));
+    }
+}
