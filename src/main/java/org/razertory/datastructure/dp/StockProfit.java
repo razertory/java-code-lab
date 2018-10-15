f09bbd664327be6c9f0d99958ce73c7c97cb60ad
chenlichun <razertory@163.com>
2018-10-15 13:30:29 +0800

    add stock problem on leetcode

---
+++ src/main/java/org/razertory/datastructure/dp/StockProfit.java
@@ -0,0 +1,9 @@
+package org.razertory.datastructure.dp;
+
+// https://leetcode.com/problems/best-time-to-buy-and-sell-stock/description/
+public class StockProfit {
+
+    int maxProfit(int[] prices) {
+        return 0;
+    }
+}
