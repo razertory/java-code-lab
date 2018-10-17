7ca864c1fc0a444569a71ec2b78d92cd7109a57f
awarker <1349028023@qq.com>
2018-10-17 10:04:20 +0800

    udpate kth bigest

--- src/main/java/org/razertory/datastructure/sort/KthBigest.java
+++ src/main/java/org/razertory/datastructure/sort/KthBigest.java
@@ -8,36 +8,43 @@ public class KthBigest {
      * @return: the Kth largest element
      */
     public int kthLargestElement(int n, int[] nums) {
-        quickSort(0, nums.length - 1, nums);
-        int key = nums.length - n;
-        return nums[key];
+        if(nums == null) {
+            return -1;
+        }
+        return quickSort(0, nums.length - 1, n, nums);;
     }
 
 
-    public static void quickSort(int lo, int hi, int[] nums) {
-        if (lo > hi) {
-            return;
+    public static void quickSort(int lo, int hi, int n, int[] nums) {
+        if (lo == hi) {
+            return nums[lo];
         }
-        int index = pos(lo, hi, nums);
-        quickSort(lo, index - 1, nums);
-        quickSort(index + 1, hi, nums);
+        int index = pos(nums, lo, hi);
+        if(index + 1 == n){
+            return nums[index];
+        }else if(index + 1 < n){
+            return sortKth(index+1, hi, n, nums);
+        }else {
+            return sortKth(lo, index -1, n, nums);
+        }
 
     }
 
     //获取中间位置
     public static int pos(int lo, int hi, int[] nums) {
-        int key = nums[lo];
-        while (lo < hi) {
-            while (nums[hi] >= key && lo < hi) {
-                hi--;
-            }
-            nums[lo] = nums[hi];
-            while (nums[lo] <= key && lo < hi) {
-                lo++;
-            }
+        int key = nums[hi];
+        while(lo < hi) {
+            while(nums[lo] >= key && lo < hi) {
+                lo ++;
+            };
             nums[hi] = nums[lo];
+            while(nums[hi] <= key && hi >lo) {
+                hi --;
+            };
+            nums[lo] = nums[hi];
+            
         }
-        nums[hi] = key;
-        return hi;
+        nums[lo] = key;
+        return lo;
     }
 }
