eaf374fe35c1cc07e468209a4d389b0619b74ce6
chenlichun <razertory@163.com>
2018-10-17 13:24:04 +0800

    sliding window

---
+++ src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
@@ -0,0 +1,10 @@
+package org.razertory.datastructure.queue;
+
+import java.util.ArrayList;
+
+// https://www.nowcoder.com/practice/1624bc35a45c42c0bc17d17fa0cba788?tpId=13&tqId=11217&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
+public class SlidingWindow {
+    public ArrayList<Integer> maxInWindows(int [] num, int size){
+        return null;
+    }
+}
