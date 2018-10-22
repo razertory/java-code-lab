bd258a8095e9847243fb89484486dab6c40f3aaa
awarker <1349028023@qq.com>
2018-10-22 21:19:59 +0800

    add sliding window annotation

--- src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
+++ src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
@@ -2,6 +2,12 @@ package org.razertory.datastructure.queue;
 
 import java.util.ArrayList;
 
+/**
+ * 滑动窗口：根据窗口大小找出当前窗口的最大值
+ *      解法一：两个循环，循环一是遍历整个数组，循环二是找出窗口中的最大值（注意边界问题，条件问题）
+ *      解法二：双端队列，继续优化？？？？？？？？？
+ */
+
 // https://www.nowcoder.com/practice/1624bc35a45c42c0bc17d17fa0cba788?tpId=13&tqId=11217&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
 public class SlidingWindow {
     public ArrayList<Integer> maxInWindows(int [] num, int size){
