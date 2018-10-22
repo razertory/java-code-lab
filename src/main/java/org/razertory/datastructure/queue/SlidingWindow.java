e7502a838e0fc79f2d6d3e021f7eceef8c069394
awarker <1349028023@qq.com>
2018-10-22 21:14:30 +0800

    update sliding window bug

--- src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
+++ src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
@@ -5,10 +5,10 @@ import java.util.ArrayList;
 // https://www.nowcoder.com/practice/1624bc35a45c42c0bc17d17fa0cba788?tpId=13&tqId=11217&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
 public class SlidingWindow {
     public ArrayList<Integer> maxInWindows(int [] num, int size){
-        if(num.length == 0 || size > num.length ) {
-            return null;
+        if(size > num.length || size == 0) {
+            return new ArrayList<Integer> ();
         }
-        ArrayList<Integer> array = new ArrayList<Integer>();
+        ArrayList<Integer> array = new ArrayList<Integer> ();
         for(int i = 0; i <= num.length - size; i++) {
             int a = 0;
             for(int j = i; j < i + size; j ++) {
