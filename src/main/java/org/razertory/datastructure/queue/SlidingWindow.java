417fa8789c7a349f0142e4bb90ced3bb583802d3
awarker <1349028023@qq.com>
2018-10-22 18:26:09 +0800

    update sliding window

--- src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
+++ src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
@@ -5,7 +5,7 @@ import java.util.ArrayList;
 // https://www.nowcoder.com/practice/1624bc35a45c42c0bc17d17fa0cba788?tpId=13&tqId=11217&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
 public class SlidingWindow {
     public ArrayList<Integer> maxInWindows(int [] num, int size){
-        if(num == null) {
+        if(num.length == 0 || size > num.length ) {
             return null;
         }
         ArrayList<Integer> array = new ArrayList<Integer>();
