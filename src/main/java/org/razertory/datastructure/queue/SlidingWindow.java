9e93cfef11c5ff0b6e9e3e0f82b8dd393902ccb4
chenlichun <razertory@163.com>
2018-10-23 10:06:45 +0800

    add change sliding window

--- src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
+++ src/main/java/org/razertory/datastructure/queue/SlidingWindow.java
@@ -4,24 +4,26 @@ import java.util.ArrayList;
 
 /**
  * 滑动窗口：根据窗口大小找出当前窗口的最大值
- *      解法一：两个循环，循环一是遍历整个数组，循环二是找出窗口中的最大值（注意边界问题，条件问题）
- *      解法二：双端队列，继续优化？？？？？？？？？
+ * 解法一：两个循环，循环一是遍历整个数组，循环二是找出窗口中的最大值（注意边界问题，条件问题）
+ * 解法二：双端队列，继续优化？？？？？？？？？
  */
 
 // https://www.nowcoder.com/practice/1624bc35a45c42c0bc17d17fa0cba788?tpId=13&tqId=11217&tPage=1&rp=1&ru=/ta/coding-interviews&qru=/ta/coding-interviews/question-ranking
 public class SlidingWindow {
-    public ArrayList<Integer> maxInWindows(int [] num, int size){
-        if(size > num.length || size == 0) {
-            return new ArrayList<Integer> ();
+    public ArrayList<Integer> maxInWindows(int[] num, int size) {
+        if (size > num.length || size == 0) {
+            return new ArrayList<Integer>();
         }
-        ArrayList<Integer> array = new ArrayList<Integer> ();
-        for(int i = 0; i <= num.length - size; i++) {
+
+        ArrayList<Integer> array = new ArrayList<Integer>();
+
+        for (int i = 0; i <= num.length - size; i++) {
             int a = 0;
-            for(int j = i; j < i + size; j ++) {
-                if(a < num[j]) {
-                    a = num[j];
-                }
+
+            for (int j = i; j < i + size; j++) {
+                a = Math.min(a, num[j]);
             }
+
             array.add(a);
         }
         return array;
