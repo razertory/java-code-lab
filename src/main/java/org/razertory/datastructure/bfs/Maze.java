f9072abd75f64a2d7514c352eca3efb2b2984dfb
Razertory <razertory@163.com>
2018-10-31 21:48:44 +0800

    maze

--- src/main/java/org/razertory/datastructure/bfs/Maze.java
+++ src/main/java/org/razertory/datastructure/bfs/Maze.java
@@ -1,4 +1,15 @@
 package org.razertory.datastructure.bfs;
 
+//https://www.lintcode.com/problem/the-maze/description
 public class Maze {
+    /**
+     * @param maze: the maze
+     * @param start: the start
+     * @param destination: the destination
+     * @return: whether the ball could stop at the destination
+     */
+    public boolean hasPath(int[][] maze, int[] start, int[] destination) {
+        // write your code here
+        return false;
+    }
 }
