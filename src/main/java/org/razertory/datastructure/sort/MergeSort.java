e6b764ea4512b5d0ff825acb6d8d16d9b6efec1c
chenlichun <razertory@163.com>
2018-12-26 13:48:54 +0800

    格式化代码

--- src/main/java/org/razertory/datastructure/sort/MergeSort.java
+++ src/main/java/org/razertory/datastructure/sort/MergeSort.java
@@ -1,74 +1,50 @@
 package org.razertory.datastructure.sort;
 
-/**
- * 归并排序<br>
- * 时间复杂度: 平均情况与最差情况都是O(nlog(n))<br>
- * 空间复杂度: It Depends
- * @author razertory
- * @see Sort
- * @see SortTest
- */
 public class MergeSort extends Sort {
 
-	public void sort(int[] array) {
-		mergeSort(array, 0, array.length - 1);
-	}
-	
-	/**
-	 * 从索引low到high归并排序数组array
-	 * @param array
-	 * @param low
-	 * @param high
-	 */
-	private void mergeSort(int[] array, int low, int high){
-		if(low < high){
-			int middle = (low + high)/2;
-			mergeSort(array, low, middle);
-			mergeSort(array, middle + 1, high);
-			
-			merge(array, low, middle, high);
-		}
-	}
-	
-	/**
-	 * 归并array
-	 * @param array
-	 * @param low
-	 * @param middle
-	 * @param high
-	 */
-	private void merge(int[] array, int low, int middle, int high){
-		// 辅助数组
-		int[] helper = new int[array.length];
-		for(int i = 0; i <= high; i++){
-			helper[i] = array[i];
-		}
-		
-		int helperLeft = low;
-		int helperRight = middle + 1;
-		int current = low;
-		
-		/**
-		 * 迭代访问helper数组，比较左右两半元素
-		 * 并将较小的元素复制到原先的数组中
-		 */
-		while(helperLeft <= middle && helperRight <= high){
-			if(helper[helperLeft] <= helper[helperRight]){
-				array[current] = helper[helperLeft];
-				helperLeft++;
-			} else{
-				array[current] = helper[helperRight];
-				helperRight++;
-			}
-			current++;
-		}
-		
-		/**
-		 * 将数组左半剩余元素复制到原先的数组中
-		 */
-		int remaining = middle - helperLeft;
-		for(int i = 0; i <= remaining; i++){
-			array[current + i] = helper[helperLeft + i];
-		}
-	}
+    public void sort(int[] array) {
+        mergeSort(array, 0, array.length - 1);
+    }
+
+    private void mergeSort(int[] array, int low, int high) {
+        if (low >= high) {
+            return;
+        }
+        int middle = (low + high) / 2;
+        mergeSort(array, low, middle);
+        mergeSort(array, middle + 1, high);
+        merge(array, low, middle, high);
+    }
+
+    /**
+     * 归并array
+     *
+     * @param array
+     * @param low
+     * @param middle
+     * @param high
+     */
+    private void merge(int[] array, int low, int middle, int high) {
+        // 辅助数组
+        int[] helper = new int[array.length];
+        System.arraycopy(array, 0, helper, 0, high + 1);
+
+        int helperLeft = low;
+        int helperRight = middle + 1;
+        int current = low;
+
+        while (helperLeft <= middle && helperRight <= high) {
+            if (helper[helperLeft] <= helper[helperRight]) {
+                array[current] = helper[helperLeft];
+                helperLeft++;
+            } else {
+                array[current] = helper[helperRight];
+                helperRight++;
+            }
+            current++;
+        }
+
+        int remaining = middle - helperLeft;
+        System.arraycopy(helper, helperLeft, array, current, remaining + 1);
+    }
 }
---
+++ src/main/java/org/razertory/datastructure/sort/ParallelSort.java
@@ -0,0 +1,9 @@
+package org.razertory.datastructure.sort;
+
+import java.util.Arrays;
+
+public class ParallelSort {
+    public void sort(int[] nums) {
+        Arrays.parallelSort(nums);
+    }
+}
